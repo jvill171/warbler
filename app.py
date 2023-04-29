3e4813d6d040cc8ec6ae546e59d26205727c117d
jvill171 <juanvillarreal64@yahoo.com>
2023-04-29 14:40:17 -0500

    Added profile edit route & form

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, EditUserForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -214,7 +214,29 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    form = EditUserForm(obj=g.user)    
+
+    if form.validate_on_submit():
+        if(User.authenticate(g.user.username, form.password.data) == g.user):
+            g.user.username = form.username.data
+            g.user.email = form.email.data
+            g.user.image_url = form.image_url.data
+            g.user.header_image_url = form.header_image_url.data
+            g.user.bio = form.bio.data
+
+            db.session.commit();
+            return redirect(f'/users/{g.user.id}')
+
+        else:
+            form.password.errors = ["Wrong password"]
+
+    return render_template('/users/edit.html', form=form)
+
+    # IMPLEMENT THIS **************************************************************************************************************
 
 
 @app.route('/users/delete', methods=["POST"])
@@ -305,7 +327,6 @@ def homepage():
 
     else:
         return render_template('home-anon.html')
-
 
 ##############################################################################
 # Turn off all caching in Flask
